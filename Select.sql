9d0e84c4d742d63e6e4480d0d1ac3197b009924b
rko2911 <ravikr.kumar50@gmail.com>
2020-06-27 15:46:13 +0530

    alter student table

--- Select.sql
+++ Select.sql
@@ -14,6 +14,9 @@ select *, CAST(cumm/tot AS NUMERIC(10,4)) ,
 ROW_NUMBER() over (order by marks desc)
 from std1
 
+
+select 
+
 select b.stdname,b.marks,b.cumm,b.perc
 FROM 
 (
@@ -24,4 +27,16 @@ cast(sum(marks) over() as numeric(10,4)) as tot
 from Student) a
 ) b
 where b.perc <0.7
+
+
+select d.stdname,d.marks2,d.cumm,d.perc2
+FROM 
+(
+select c.stdname,c.marks2,c.cumm,(c.cumm/c.tot) as perc2 from
+(select stdname,marks2,sum(marks2)
+over(order by marks2 desc) as cumm,
+cast(sum(marks2) over() as numeric(10,4)) as tot 
+from Student) c
+) d
+where d.perc2 <0.7
 
